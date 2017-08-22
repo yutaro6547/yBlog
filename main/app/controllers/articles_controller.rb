58d3dfab69008746cd51928e503b78f5867a6c0c
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2017-08-22 15:24:32 +0900

    記事作成時にタイトル未入力及び最小５文字以上入力しなかった場合データベースに保存されないようにした

--- main/app/controllers/articles_controller.rb
+++ main/app/controllers/articles_controller.rb
@@ -5,8 +5,11 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -16,7 +19,7 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
