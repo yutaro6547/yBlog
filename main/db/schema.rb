9b3b7ae42c3b490a4da715b62a2deae98cf8a74f
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2017-08-22 14:50:19 +0900

    マイグレーションファイル生成

--- main/db/schema.rb
+++ main/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820064001) do
+ActiveRecord::Schema.define(version: 20170822054801) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string "name"
