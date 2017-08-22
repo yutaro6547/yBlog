434af09a081ca455df70a8b71c7fbafd9d6ae0ff
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2017-08-22 21:18:52 +0900

    テスト的にHTTP認証をつけてアクセス制限をするようにした

--- main/app/controllers/articles_controller.rb
+++ main/app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "test",
+                               password: "secret",
+                               except: [:index, :show]
+
   def new
     @article = Article.new
   end
